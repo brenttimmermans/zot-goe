f48031d3616324fe60beab63fd1a931ab3ea5489
Brent Timmermans <brenttimmermans7@gmail.com>
2024-10-17 20:58:27 +0200

    ✨ add link to photos website

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
 import styles from "./page.module.css";
 
+const PHOTOS_BY_BRENT_LINK = "https://photos.brenttimmermans.com";
+
 const Home = () => (
   <main className={styles.main}>
     <Construction />
@@ -9,7 +11,15 @@ const Home = () => (
 const Construction = () => (
   <div className={styles.construction}>
     <h2>🚧 Something Zot goe (👀) is coming 🚧</h2>
-    <p className={styles.subtle}>Check back later</p>
+    <p className={styles.subtle}>
+      While you&apos;re here, you can always check out some of my photo&apos;s
+      over
+      <br />
+      at{" "}
+      <a href={PHOTOS_BY_BRENT_LINK} className={styles.link}>
+        photos.brenttimmermans.com
+      </a>
+    </p>
   </div>
 );
 
